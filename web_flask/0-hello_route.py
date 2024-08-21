a1165abf02b534894a35454e50af16b9eb2e3681
Tigo-cmd <ja16162040@gmail.com>
2024-08-21 14:58:48 +0100

    AirBNB_Clone_V2

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,18 @@
+#!/usr/bin/python3
+"""A script that starts flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+"""
+
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def display():
-    return "Hello HBNB!"
+@app.route('/')
+def hello_flask():
+    """Return string when route queried
+    """
+    return 'Hello HBNB!'
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
